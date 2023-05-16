b50a63baeab0cfd2c760edb38ff588b54b32c0d3
Daniel Palmer <danp1995mail@gmail.com>
2023-05-15 21:44:40 -0700

    Update pdf-qa.ts

--- chains/pdf-qa.ts
+++ chains/pdf-qa.ts
@@ -1,7 +1,7 @@
 import { defineChain } from '@relevanceai/chain';
 
 // the dataset created in this Google Colab: https://colab.research.google.com/drive/17CGlVUzcE3kz0Oqy4qmi33CJPuTlLaav?usp=sharing
-const DATASET_ID = 'my_pdfs';
+const DATASET_ID = 'knowledge_base';
 const DATASET_EMBEDDING_FIELD = 'embedding_vector_';
 
 const chain = defineChain({
